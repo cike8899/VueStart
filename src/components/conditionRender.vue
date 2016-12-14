dc1610192651b092797e5c43881fc32516f4157a
cike8899 <18267850322.com>
2016-12-14 22:47:18 +0800

    Conditional Rendering

---
+++ src/components/conditionRender.vue
@@ -0,0 +1,30 @@
+<template v-if="ok">
+    <div>
+        <h1 v-if="ok">Yes</h1>
+        <h1 v-else>No</h1>
+        <h1>Title</h1>
+        <p>Paragraph 1</p>
+        <p>Paragraph 2</p>
+        <div v-if="Math.random() > 0.5">
+            Sorry
+        </div>
+        <div v-else>
+            Not sorry
+        </div>
+        <h1 v-show="ok">Hello!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+           ok:true
+        }
+    }
+}    
+</script>
+
+<style>
+
+</style>
--- src/route/index.js
+++ src/route/index.js
@@ -3,8 +3,9 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import Hello from '../components/Hello.vue'
-import Tpl from '../components/tpl.vue';
-import Computed from '../components/computedAttr.vue';
+import Tpl from '../components/tpl.vue'
+import Computed from '../components/computedAttr.vue'
+import Condition from '../components/conditionRender.vue';
 
 export default new VueRouter(
     {
@@ -32,6 +33,13 @@ export default new VueRouter(
                 name: 'computed',
                 components: {
                     default: Computed
+                }
+            },
+            {
+                path: '/condition',
+                name: 'condition',
+                components: {
+                    default: Condition
                 }
             }
         ]
