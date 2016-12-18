c1e2426fdf992dda520456885bb2434d8682733c
cike8899 <18267850322.com>
2016-12-18 17:31:08 +0800

    format code from my personal pc

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+<div id="app">
+  <img src="./assets/logo.png">
+  <router-view></router-view>
+</div>
 </template>
-
 <script>
 import Hello from './components/Hello'
 
 export default {
   name: 'app',
-  data (){
+  data() {
     return {}
   }
 }
 </script>
-
-<style>
-  #app {
+<style lang="less">
+#app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+    -moz-osx-font-smoothin: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import router from './route/index';
+import router from './route/index'
 import App from './App'
 
 /* eslint-disable no-new */
 const app = new Vue({
-  ...App,
-  router
+	...App,
+	router
 })
 
 app.$mount('#app')
